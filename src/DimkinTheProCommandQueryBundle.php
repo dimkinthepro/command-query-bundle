3916e39d56cd4c660d57191d4bfb3db03d3f0ce7
Dimkin The PRO <dimkin.the.pro@gmail.com>
2023-05-23 16:37:54 +0500

    Fix bundle config

---
+++ src/DimkinTheProCommandQueryBundle.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DimkinThePro\CommandQuery;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class DimkinTheProCommandQueryBundle extends Bundle
+{
+}
